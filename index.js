866faaf5c76a53d0102e56531654d243498c93c8
ahmisrafil <ahmisrafil@yahoo.com>
2023-11-14 03:28:59 +0600

    foodDB created and api created for adding food to databse

--- index.js
+++ index.js
@@ -11,28 +11,50 @@ app.use(express.json());
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dwvikya.mongodb.net/?retryWrites=true&w=majority`;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gur30er.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        // add food to database
+        const foodCollection = client.db("foodDB").collection("food");
+        app.post('/food', async (req, res) => {
+            const newFood = req.body;
+            console.log(newFood);
+            const result = await foodCollection.insertOne(newFood);
+            res.send(result);
+        })
+
+        //read food data from database
+        app.get('/food', async (req, res) => {
+            const cursor = foodCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
@@ -42,16 +64,74 @@ run().catch(console.dir);
 
 
 
+// .................................//
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dwvikya.mongodb.net/?retryWrites=true&w=majority`;
+
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+
+//  // add food to database
+//  const foodCollection = client.db("foodDB").collection("food");
+//  app.post('/food', async(req,res)=>{
+//      const newFood = req.body;
+//      console.log(newFood);
+//      const result = await productCollection.insertOne(newFood);
+//      res.send(result);
+//  })
+
+//  //read food data from database
+//  app.get('/food', async(req, res)=>{
+//      const cursor = foodCollection.find();
+//      const result = await cursor.toArray();
+//      res.send(result);
+//  })
 
 
 
 
-app.get('/', (req, res)=>{
+
+
+
+
+
+
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     // await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
+
+
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
     res.send('server is running')
 })
 
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`food unity server is running on port ${port}`);
 
 })
