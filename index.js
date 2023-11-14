34099e08ddeb1d0d943f33b379f58a3be4305019
ahmisrafil <ahmisrafil@yahoo.com>
2023-11-15 00:44:53 +0600

    api created to read single food data

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 //middleware
@@ -45,6 +45,14 @@ async function run() {
         })
 
 
+        //read data of a specific id
+        app.get('/food/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query ={_id : new ObjectId(id)};
+            const food = await foodCollection.findOne(query);
+            res.send(food);
+        })
+
 
 
 
@@ -57,68 +65,6 @@ async function run() {
     }
 }
 run().catch(console.dir);
-
-
-
-
-
-
-
-// .................................//
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dwvikya.mongodb.net/?retryWrites=true&w=majority`;
-
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
-
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-
-//  // add food to database
-//  const foodCollection = client.db("foodDB").collection("food");
-//  app.post('/food', async(req,res)=>{
-//      const newFood = req.body;
-//      console.log(newFood);
-//      const result = await productCollection.insertOne(newFood);
-//      res.send(result);
-//  })
-
-//  //read food data from database
-//  app.get('/food', async(req, res)=>{
-//      const cursor = foodCollection.find();
-//      const result = await cursor.toArray();
-//      res.send(result);
-//  })
-
-
-
-
-
-
-
-
-
-
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     // await client.close();
-//   }
-// }
-// run().catch(console.dir);
-
-
-
-
 
 
 
